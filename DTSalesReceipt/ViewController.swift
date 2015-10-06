7b6f1342dd3eda6baef25304129d7e9b0b404a0c
Oliver Drobnik <oliver@cocoanetics.com>
2015-10-06 14:46:26 +0200

    Initial Commit

---
+++ DTSalesReceipt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DTSalesReceipt
+//
+//  Created by Oliver Drobnik on 06/10/15.
+//  Copyright © 2015 Oliver Drobnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
