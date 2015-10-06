9d9020828604c11baa43d89c4cce65e8c09c285a
Oliver Drobnik <oliver@cocoanetics.com>
2015-10-06 19:30:49 +0200

    Renamed project to Kvitto

---
+++ Core/Kvitto.h
@@ -0,0 +1,19 @@
+//
+//  Kvitto.h
+//  Kvitto
+//
+//  Created by Oliver Drobnik on 06/10/15.
+//  Copyright © 2015 Oliver Drobnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kvitto.
+FOUNDATION_EXPORT double KvittoVersionNumber;
+
+//! Project version string for Kvitto.
+FOUNDATION_EXPORT const unsigned char KvittoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kvitto/PublicHeader.h>
+
+
